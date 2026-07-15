0bb690fbd87d9ef6823b29ff7776fffabe875c63
root <root@mail.avispotters.net>
2026-07-15 15:52:26 +0800

    changed to get or create instead of add

--- submissions/judge.py
+++ submissions/judge.py
@@ -428,7 +428,7 @@ def finalize_submission(submission, case_results, max_runtime,
         submission.status = "Accepted"
         submission.save(update_fields=["status", "runtime", "memory"])
         if problem not in submission.user.solved_problems.all():
-            submission.user.solved_problems.add(problem)
+            submission.user.solved_problems.get_or_create(problem)
 
 
 def _case_status_from_error(error, actual, expected):
